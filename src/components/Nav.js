7095910f8faa88da952ef4a0010452f364157cca
Ogechi Ikwunagu <54102855+ogechinagu@users.noreply.github.com>
2022-12-21 13:11:22 +0000

    Updates

---
+++ src/components/Nav.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export const Nav = () => {
+  return (
+    <nav>
+          <ul>
+            <li>
+              <Link to='/'>Home</Link>
+            </li>
+            <li>
+              <Link to='/super-heroes'>Traditional Super Heroes</Link>
+            </li>
+            <li>
+              <Link to='/rq-super-heroes'>RQ Super Heroes</Link>
+            </li>
+          </ul>
+        </nav>
+  )
+}
